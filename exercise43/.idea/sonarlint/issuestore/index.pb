b1932ae7c1d11cd901b6a417ec0f4f341ecfe907
Blake Lochmandy <blakeloch@knights.ucf.edu>
2021-10-11 18:03:03 -0400

    Add exercise43 OutputClass code (in progress)

--- exercise43/.idea/sonarlint/issuestore/index.pb
+++ exercise43/.idea/sonarlint/issuestore/index.pb
@@ -4,8 +4,8 @@
 G
 docs/Class Diagram.puml,e/2/e2792763866d223d37c30ecf4f01550e6cdead94
 V
-&src/main/java/baseline/InputClass.java,1/d/1d5a34b886947090b98ec1dacff0d4634b2647d5
-V
 &src/main/java/baseline/Solution43.java,b/4/b4347542199e6f8f4a0c2a06abf7605a025d7459
+V
+&src/main/java/baseline/InputClass.java,1/d/1d5a34b886947090b98ec1dacff0d4634b2647d5
 W
 'src/main/java/baseline/OutputClass.java,1/0/102ba91fae0c808114344672c4ea4e39e730984a
--- exercise43/src/main/java/baseline/OutputClass.java
+++ exercise43/src/main/java/baseline/OutputClass.java
@@ -5,30 +5,67 @@
 
 package baseline;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class OutputClass {
   
   public void createWebFolder(String siteName) {
     // create folder website/siteName
-    // print "Created ./website/'siteName'"
+    File webFolder = new File("website"+File.separator+siteName);
+    if(webFolder.mkdirs()) {
+      // print "Created ./website/'siteName'"
+      System.out.println("Created ./website/" + siteName);
+    } else {
+      // print "Directory already exists"
+      System.out.println("Directory already exists");
+    }
   }
 
   public void createIndexHtml(String siteName, String author) {
     // create file website/'siteName'/index.html
-    // write <title>"'siteName'"<\title> to index.html
-    // write <meta>"'author'"<\meta> to index.html
-    // print "Created ./website/'siteName'/index.html"
+    try (FileWriter output = new FileWriter("website"+File.separator+siteName+File.separator+"index.html")) {
+      // write "<title>"'siteName'"<\title>" to index.html
+      output.write("<title>"+siteName+"<\\title>\n");
+      // write "<meta>"'author'"<\meta>" to index.html
+      output.write("<author>"+author+"<\\meta>");
+      // print "Created ./website/'siteName'/index.html"
+      System.out.println("Created ./website/" + siteName + "/index.html");
+      // close file
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
-  public void createJavaScriptFolder(String siteName, Boolean javaScriptFolder) {
+  public void createJavaScriptFolder(String siteName, boolean javaScriptFolder) {
     // if javaScriptFolder is true
-    // create folder website/'siteName'/js
-    // print "Created ./website/'siteName'/js"
+    if (javaScriptFolder) {
+      // create folder website/'siteName'/js
+      File webFolder = new File("website"+File.separator+siteName+File.separator+"js");
+      if(webFolder.mkdirs()) {
+        // print "Created ./website/'siteName'/js"
+        System.out.println("Created ./website/" + siteName + "/js");
+      } else {
+        // print "Directory already exists"
+        System.out.println("Directory already exists");
+      }
+    }
   }
 
-  public void createCssFolder(String siteName, Boolean cssFolder) {
+  public void createCssFolder(String siteName, boolean cssFolder) {
     // if cssFolder is true
-    // create folder website/'siteName'/css
-    // print "Created ./website/'siteName'/css"
+    if (cssFolder) {
+      // create folder website/'siteName'/css
+      File webFolder = new File("website"+File.separator+siteName+File.separator+"css");
+      if(webFolder.mkdirs()) {
+        // print "Created ./website/'siteName'/css"
+        System.out.println("Created ./website/" + siteName + "/css");
+      } else {
+        // print "Directory already exists"
+        System.out.println("Directory already exists");
+      }
+    }
   }
 
 }
