07856e86e16a87e9fd82d3f9eef5bf738dfb5784
Blake Lochmandy <blakeloch@knights.ucf.edu>
2021-10-11 23:22:19 -0400

    Add exercise44 abstract pseudocode

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,17 @@
+package baseline;
+
+public class Solution44 {
+
+  public static void main(String[] args) {
+
+    // import data from json to list of maps
+
+    // get search string from user
+
+    // search by search string
+      // if found print info
+      //if not prompt again
+
+  }
+
+}
