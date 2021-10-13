38f987a5d02539745e2f74d313f695f94fbb17f1
Blake Lochmandy <blakeloch@knights.ucf.edu>
2021-10-13 03:09:09 -0400

    Add comprehensive tests for exercise43

--- exercise43/src/test/java/baseline/OutputClassTest.java
+++ exercise43/src/test/java/baseline/OutputClassTest.java
@@ -7,11 +7,30 @@ package baseline;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class OutputClassTest {
+
+  private String readFile(String file) {
+
+    // read file as a string
+    String data = "";
+    try {
+      data = Files.readString(Path.of(file));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    // return the string
+    return data;
+
+  }
 
   @Test
   void buildIndexHtml() {
@@ -28,4 +47,69 @@ class OutputClassTest {
     assertEquals(expected, actual);
   }
 
+  @Test
+  void createWebFolder() {
+    OutputClass output = new OutputClass();
+    String siteName = "testSite";
+    output.createWebFolder(siteName);
+    assertTrue(Files.exists(Path.of("data/website" + File.separator + siteName)));
+  }
+
+  @Test
+  void createIndexHtmlExists() {
+    OutputClass output = new OutputClass();
+    String siteName = "testSite";
+    output.createWebFolder(siteName);
+    List<String> indexHtml = new ArrayList<>();
+    indexHtml.add("<!DOCTYPE html>");
+    indexHtml.add("<head>");
+    indexHtml.add("\t<title>"+"siteName"+"</title>");
+    indexHtml.add("\t<meta author=\""+"author"+"\">");
+
+    output.createIndexHtml(siteName, indexHtml);
+    assertTrue(Files.exists(Path.of("data/website" + File.separator + siteName + File.separator+"index.html" )));
+
+  }
+
+  @Test
+  void createIndexHtmlWrites() {
+    OutputClass output = new OutputClass();
+    String siteName = "testSite";
+    output.createWebFolder(siteName);
+    String expected = """
+        <!DOCTYPE html>
+        <head>
+        \t<title>testSite</title>
+        \t<meta author="Author">
+        """;
+
+    List<String> indexHtml = new ArrayList<>();
+    indexHtml.add("<!DOCTYPE html>");
+    indexHtml.add("<head>");
+    indexHtml.add("\t<title>testSite</title>");
+    indexHtml.add("\t<meta author=\"Author\">");
+
+    output.createIndexHtml(siteName, indexHtml);
+
+    String actual = readFile("data/website/testSite/index.html");
+
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  void createJavaScriptFolder() {
+    OutputClass output = new OutputClass();
+    String siteName = "testSite";
+    output.createJavaScriptFolder(siteName, true);
+    assertTrue(Files.exists(Path.of("data/website" + File.separator + siteName+File.separator+"js")));
+  }
+
+  @Test
+  void createCssFolder() {
+    OutputClass output = new OutputClass();
+    String siteName = "testSite";
+    output.createCssFolder(siteName, true);
+    assertTrue(Files.exists(Path.of("data/website" + File.separator + siteName+File.separator+"css")));
+  }
 }
